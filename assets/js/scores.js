92d54296a9f10db5925b679ba43b01a866d96b2a
CcMong <fullstackcodex@gmail.com>
2023-01-19 13:19:31 +0000

    Working in scores.js, created a function to sort a list element in descending order based on data index.

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,1 +1,24 @@
 
+var clearButtonEl = document.querySelector("#clear");
+
+// Function to sort a list element, accessed by its ID, in descending order by using its data-index
+
+function sortList(listId) { 
+
+    var orderedList = document.getElementById(listId);
+  
+    var indexes = document.querySelectorAll("[data-index]");
+  
+    var arrayOfIndexes = Array.from(indexes);
+  
+    var sortedArray = arrayOfIndexes.sort(function(a, b) {
+        if (a.dataset.index < b.dataset.index)
+        return 1;
+        if (a.dataset.index > b.dataset.index)
+        return -1;
+        return 0;
+    });
+
+    sortedArray.forEach(li => orderedList.appendChild(li));
+}
+
