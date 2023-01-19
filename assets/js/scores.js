a80c589e19a88557a4b3b3061059ef3b897db7f3
CcMong <fullstackcodex@gmail.com>
2023-01-19 13:38:30 +0000

    Wrote code to populate the high score leaderboard, and then sort it in descending order.

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -22,3 +22,41 @@ function sortList(listId) {
     sortedArray.forEach(li => orderedList.appendChild(li));
 }
 
+/*Populate the High Score Leaderboard with User Scores
+=======================================================*/
+
+var highScoresList = document.querySelector("#highscores"); // Reference the ordered list to contain the high scores
+
+// First get all scores from local storage and convert them to array/object form
+
+var highScores = JSON.parse(localStorage.getItem("scores")); 
+
+// Then loop through the array of objects. For each array element, ie. a score object:
+
+highScores.forEach((score) => {
+
+    // Create a <li> element
+
+    var listItem = document.createElement("li");
+
+    // Print in the initials and score information
+
+    listItem.textContent = `${score.initials}: ${score.score}`;
+
+    // Set an attribute, data index, which will be equal to the score
+
+    listItem.setAttribute("data-index", score.score); 
+
+    // And then append the populated <li> element to the ordered list that will contain the high scores
+
+    highScoresList.appendChild(listItem);
+    
+})
+
+// Now call the function to sort the scores in descending order, after the most recent scores are logged
+
+sortList("highscores");
+
+// To only show 10 scores on the highscore board, I have decided to use CSS to make sure only the top 10 list items are displayed
+
+
